126ec15e55c70faa33bc367ddf54172373964469
Caleb Covington <calebinfour@gmail.com>
2023-06-09 12:01:59 -0500

    Added structure for delete API for the friend table

--- api.py
+++ api.py
@@ -12,7 +12,20 @@ api = Blueprint('api', __name__)
 def friend_list():
     connection = dbConnection()
     cursor = connection.cursor(dictionary=True)
-    sql_query = "SELECT * FROM friend"
+    sql_query = "SELECT firstname, lastname FROM friend"
     cursor.execute(sql_query)
     rows = cursor.fetchall()
     return jsonify(rows)
+
+# request to delete a friend from the friends table
+@api.route('/removefriend/<int:id>', methods = ['DELETE'])
+def remove_friend(id):
+    connection = dbConnection()
+    cursor = connection.cursor(dictionary=True)
+    sql_query = "DELETE FROM friend WHERE id = %s"
+    cursor.execute(sql_query, (id,))
+    connection.commit()
+    return jsonify(message="Friends come and go.")
+    
+
+    
